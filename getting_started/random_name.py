6f7442b8fbb0600d79ec53a5e9baab42c000454b
Davis, Chris <Chris.Davis@viacom.com>
2021-05-25 07:42:17 -0400

    random name testing

---
+++ getting_started/random_name.py
@@ -0,0 +1,7 @@
+import names
+
+def name_male():
+    return names.get_first_name(gender='male')
+
+def full_name():
+    return names.get_full_name()
--- tests/test_getting_started/test_random_name.py
+++ tests/test_getting_started/test_random_name.py
@@ -1,12 +1,13 @@
 import random
 import pytest
+from getting_started.random_name import name_male, full_name
 
 ## Here we are not calling out any application code. Just demonstrating the fixure decorator.
 ## As I read this, the test assertion is accepting any value.  So, when we manually send None data, the test fails.
 
 @pytest.fixture
 def random_name():
-    names = ['Jphn', 'Jane', 'Marry']
+    names = ['John', 'Jane', 'Marry']
     # return None
     return random.choice(names)
 
@@ -14,3 +15,12 @@ def random_name():
 def test_fixture_usage(random_name):
     assert random_name
 
+
+def test_name_male():
+    assert type(name_male()) is str
+    assert len(name_male()) > 0
+
+def test_full_name():
+    assert type(full_name()) is str
+    assert '' in full_name()
+    assert len(full_name()) > 0
